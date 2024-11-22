554386eeaa8da2b2f8e3e411421897cfb6107e4f
dylan <D00265421@Student.dkit.ie>
2024-11-22 12:46:59 +0000

    Project creation

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -3,4 +3,35 @@ public class Cylinder
     private double radius;
     private double height;
     private double width;
+
+    public Cylinder(double radius, double height, double width)
+    {
+        this.radius = radius;
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
